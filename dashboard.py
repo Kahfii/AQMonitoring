ba795de27127dfb908190c7ec6c24a815d0567ea
Rahman Ilyas Al Kahfi <ilyasalkahfi98@gmail.com>
2024-07-27 18:40:18 +0700

    fix(dashboard): tambahkan loading saat saran kesehatan dimuat

--- dashboard.py
+++ dashboard.py
@@ -6,6 +6,7 @@ import pycountry
 import json
 import altair as alt
 from streamlit_option_menu import option_menu
+import time
 
 url = 'http://localhost:11434/api/chat'
 headers = {'Content-Type': 'application/json'}
@@ -23,18 +24,17 @@ def classify_air_quality(value, unit):
     else:
         return 'Tidak Diketahui'
 
-
 def create_prompt(hasil_prediksi, country):
-    if hasil_prediksi == 'healthy':
-        prompt = f"""Berikan kegiatan yang cocok dilakukan saat kualitas udara '{hasil_prediksi}'  di {country}.
+    if hasil_prediksi == 'Healthy':
+        prompt = f"""Berikan kegiatan yang cocok dilakukan saat kualitas udara {hasil_prediksi} di {country}.
         
         Hasil Prediksi Udara adalah {hasil_prediksi}.
         
         Buat menjadi singkat dan komunikatif, dalam bahasa indonesia dan maksimum 300 karakter. dengan frasa pertama selalu dimulai dengan
         'Hasil klasifikasi udara di {country} menunjukkan kualitas {hasil_prediksi}'
         """
-    elif hasil_prediksi == 'normal':
-        prompt = f"""Berikan kegiatan yang cocok dilakukan saat kualitas udara '{hasil_prediksi}'  di {country}. jelaskan juga saran kesehatan pada saat beraktivitas di kondisi udara yang '{hasil_prediksi}'. Ingatkan agar selalu tetap menjaga kesehatan
+    elif hasil_prediksi == 'Normal':
+        prompt = f"""Berikan kegiatan yang cocok dilakukan saat kualitas udara {hasil_prediksi} di {country}. jelaskan juga saran kesehatan pada saat beraktivitas di kondisi udara yang {hasil_prediksi}. Ingatkan agar selalu tetap menjaga kesehatan
         
         Hasil Prediksi Udara adalah {hasil_prediksi}.
         
@@ -42,29 +42,42 @@ def create_prompt(hasil_prediksi, country):
         'Hasil klasifikasi udara di {country} menunjukkan kualitas {hasil_prediksi}'
         """
     else:
-        prompt = f"""Berikan penjelasan mengenai dampak dan penyakit yang mungkin terjadi pada kesehatan manusia jika terpapar polusi udara dengan kualitas '{hasil_prediksi}'.
+        prompt = f"""Berikan penjelasan mengenai dampak dan penyakit yang mungkin terjadi pada kesehatan manusia jika terpapar polusi udara dengan kualitas {hasil_prediksi}.
         
         Hasil Prediksi Udara adalah {hasil_prediksi}.
         
-        Buat menjadi singkat dan komuikatif, dalam bahasa indonesia dan maksimum 300 karakter. dengan frasa pertama selalu dimulai dengan
+        Buat menjadi singkat dan komunikatif, dalam bahasa indonesia dan maksimum 300 karakter. dengan frasa pertama selalu dimulai dengan
         'Hasil klasifikasi udara di {country} menunjukkan kualitas {hasil_prediksi}'
         """
     
     sent_json = {"model": "llama3",
                  "messages": [{"role": "user", "content": prompt}],
-                 "stream": False
+                 "stream": True
                 }
     return sent_json
-
 
 def prediction_air_quality(temp, humidity, ppm):
     prediction = model.predict([[temp, humidity, ppm]])
     return prediction[0]
 
-def extract_relevant_text(response_text):
-    lines = response_text.split('\n')
-    relevant_lines = [line for line in lines if line.strip() and not line.startswith(('Here'))]
-    return '\n'.join(relevant_lines)
+def stream_response(response):
+    response_text = ""
+    for line in response.iter_lines():
+        if line:
+            decoded_line = line.decode("utf-8")
+            try:
+                json_response = json.loads(decoded_line)
+                if "message" in json_response:
+                    response_text += json_response["message"]["content"]
+                    yield json_response["message"]["content"]
+            except json.JSONDecodeError:
+                pass
+    yield response_text
+
+def animate_typing(placeholder, text, delay=0.05):
+        for i in range(len(text) + 1):
+                placeholder.markdown(text[:i])
+                time.sleep(delay)
 
 def main():
     try:
@@ -100,51 +113,34 @@ def main():
         
         Unit pengukuran polutan udara:
         - **ppm (parts per million):** Digunakan untuk mengukur konsentrasi gas polutan.
-        """) 
+        """)
 
     # Air Quality Monitor page
     elif selected == "Air Quality Monitor":
         st.title("World Air Quality Monitor")
         
-        
         sorted_countries = df['Country Label'].sort_values().unique()
-
-        
         country = st.selectbox('Pilih Negara', sorted_countries)
-
-        
         country_data = df[df['Country Label'] == country]
-
         
         st.dataframe(country_data[['City', 'Location', 'Pollutant', 'Value', 'Unit', 'Klasifikasi Kualitas Udara']])
-
-        
         air_quality_summary = country_data['Klasifikasi Kualitas Udara'].value_counts()
         st.write('Ringkasan Kualitas Udara:')
         st.bar_chart(air_quality_summary)
-
-    
-        city = st.selectbox('Pilih Kota', country_data['City'].unique())
-
         
+        city = st.selectbox('Pilih Kota', country_data['City'].unique())
         city_data = country_data[country_data['City'] == city]
-
         
         st.write(f'Detail Kualitas Udara di {city}:')
         st.dataframe(city_data[['Pollutant', 'Value', 'Unit', 'Klasifikasi Kualitas Udara']])
 
-        st.write("")
-        st.write("")
-        st.write("")
-        st.write("")
-
         chart = alt.Chart(df).mark_bar().encode(
-        x=alt.X('Country Label:O', sort=None),
-        y='count():Q',
-        color='Klasifikasi Kualitas Udara:N',
-        tooltip=['Country Label:N', 'count():Q', 'Klasifikasi Kualitas Udara:N']
+            x=alt.X('Country Label:O', sort=None),
+            y='count():Q',
+            color='Klasifikasi Kualitas Udara:N',
+            tooltip=['Country Label:N', 'count():Q', 'Klasifikasi Kualitas Udara:N']
         ).properties(
-        title='Jumlah Data Kualitas Udara Berdasarkan Negara'
+            title='Jumlah Data Kualitas Udara Berdasarkan Negara'
         ).interactive()
 
         st.altair_chart(chart, use_container_width=True)
@@ -152,10 +148,9 @@ def main():
     # Test Sensor page
     elif selected == "Test Sensor":
         st.title("Test Sensor")
-        st.header("Masukkan Nilai Sensor")
+        st.header("Hasil Pembacaan Sensor")
 
-        
-        response = requests.get('http://IP4_ADDRESS:5000/get-data')
+        response = requests.get('http://192.168.1.6:5000/get-data')
         if response.status_code == 200:
             data = response.json()
             df = pd.DataFrame(data)
@@ -163,9 +158,8 @@ def main():
             st.error("Gagal mengambil data dari server")
             return
 
-        selected_row = st.selectbox('Pilih Data Sensor', df.index)
-        selected_data = df.iloc[selected_row]
-        st.write("Data yang Dipilih:")
+        selected_data = df.iloc[-1]
+        st.write("Data :")
         st.write(selected_data)
 
         countries = [country.name for country in pycountry.countries]
@@ -177,7 +171,6 @@ def main():
             ppm = selected_data['ppm']
             result = prediction_air_quality(temp, humidity, ppm)
             
-        
             if result == 'healthy':
                 st.success(f"Kualitas Udara: {result}")
             elif result == 'normal':
@@ -185,21 +178,25 @@ def main():
             else:
                 st.error(f"Kualitas Udara: {result}")
             
-    
-            response = requests.post(url, json=create_prompt(result, country), headers=headers)
-            if response.status_code == 200:
-                result = json.loads(response.content)['message']['content']
-                st.write("Saran Kesehatan dari Llama3:")
-                st.write(result)
-            else:
-                st.write("Server sedang dalam gangguan")
+            with st.spinner("Mohon Tunggu Saran Kesehatan"):
+                response = requests.post(url, json=create_prompt(result, country), headers=headers, stream=True)
+
+                if response.status_code == 200:
+                    placeholder = st.empty()
+                    final_response = ""
+                    for content in stream_response(response):
+                        animate_typing(placeholder, content)
+                        final_response += content
+
+                else:
+                    st.write("Server sedang dalam gangguan")
 
     # Profile page
     elif selected == "Profile":
         st.title("Profile Pengembang Aplikasi")
         st.header("Pengembang 1 👩🏻‍💻")
         st.write("""
-        Nama: Adinda Rizki Sya'bana Diva
+        Nama: aaaa
         
         Peran: Data Scientist
                 
@@ -208,7 +205,7 @@ def main():
 
         st.header("Pengembang 2 👩🏻‍💻")
         st.write("""
-        Nama: Salwa Nafisa
+        Nama: aaaa
                 
         Peran: Frontend Developer
                 
@@ -217,7 +214,7 @@ def main():
 
         st.header("Pengembang 3 👨🏻‍💻")
         st.write("""
-        Nama: Rahman Ilyas Al Kahfi
+        Nama: aaaa
                 
         Peran: Backend Developer dan IOT Engineer
                 
